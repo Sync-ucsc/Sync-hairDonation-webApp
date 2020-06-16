e18c133fa2bb2c17e0970062123955c98fd46684
SithijaSiriwardana <sithijalawan@gmail.com>
2020-06-16 20:29:46 +0530

    module.ts upadated

--- src/app/donor-layout/donor-layout.module.ts
+++ src/app/donor-layout/donor-layout.module.ts
@@ -6,6 +6,14 @@ import { DonorLayoutRoutes } from './donor-layout.routing';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DonorRequestComponent } from './donor-request/donor-request.component';
 import { BookAppointmentComponent } from './book-appointment/book-appointment.component';
+import { AgmCoreModule } from '@agm/core';
+import { HttpClientModule } from '@angular/common/http';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
 
 
 @NgModule({
@@ -13,11 +21,21 @@ import { BookAppointmentComponent } from './book-appointment/book-appointment.co
     CommonModule,
     RouterModule.forChild(DonorLayoutRoutes),
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAkGlhRjMfmotb0UBMf8EAcmkTB6v3WEVM',
+      libraries: ['places']
+    }),
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatDialogModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    NgxSpinnerModule
   ],
   declarations: [
     DashboardComponent,
     DonorRequestComponent,
-	BookAppointmentComponent,
+	  BookAppointmentComponent,
   ]
 })
 
