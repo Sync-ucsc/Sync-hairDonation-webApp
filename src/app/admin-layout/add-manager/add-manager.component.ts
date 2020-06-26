bada2a8180c9a8d848284401d943cfb530e63f67
SachinduHimash <himash1997@gmail.com>
2020-06-26 09:25:08 +0530

    Added front end of manager cruds

--- src/app/admin-layout/add-manager/add-manager.component.ts
+++ src/app/admin-layout/add-manager/add-manager.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ManagerApiService } from './../../service/manager-api.service'
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-add-manager',
@@ -6,10 +11,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-manager.component.scss']
 })
 export class AddManagerComponent implements OnInit {
+  
+ submitted=false;
 
-  constructor() { }
+  managerForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telephone: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    address: new FormControl('', Validators.required),
+
+  })
+  
+
+  constructor(
+    private apiService:ManagerApiService,
+    private router:Router
+
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log(this.managerForm.value);
+    this.submitted = true;
+
+    if (!this.managerForm.valid) {
+      return false;
+    } else {
+
+      this.apiService.createManager(this.managerForm.value).subscribe(
+        data => {
+          console.log('Manager successfully created!' + data)
+          Swal.fire(
+            'Done!',
+            'You added a new manager!',
+            'success'
+          )
+          this.router.navigateByUrl('/admin/view-manager');
+        },
+        error => {
+          // Do something with error
+          console.error(error);
+          Swal.fire(
+            'error!',
+            'Adding manager failed!',
+            'error'
+          )
+        }
+      );
+    }
+  }
 }
+
---
+++ src/app/model/manager.ts
@@ -0,0 +1,8 @@
+export class Manager {
+    name: string;
+    email: string;
+    telephone: number;
+    address: string;
+    
+
+}
