6e198f96a1e842d47e2157a36604d83f14c644ac
tharinduvindula <tharinduvindula@gmail.com>
2020-06-27 18:18:36 +0530

    doner signup ui complete

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,11 @@
 /// <reference types="@types/googlemaps" />
-import * as io from 'socket.io-client';
 import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormGroupDirective, NgForm, FormBuilder } from '@angular/forms';
 import { MapsAPILoader, MouseEvent } from '@agm/core';
 import { Router } from '@angular/router';
 declare function getFingerprint(): any;
+import PlaceResult = google.maps.places.PlaceResult;
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-signup',
@@ -16,14 +17,23 @@ export class SignupComponent implements OnInit {
   user = {
     fname:'',
     lname:'',
-    phone:'',
     email:'',
-    lat: '',
-    address:'',
+    nic:'',
+    phone: '',
+    address: '',
+    lat: 0,
+    lon: 0,
+    address1: '',
+    password:''
   }
 
-  latitude =5;
-  longitude = 5;
+  myform: FormGroup;
+  showDetails = true;
+  matcher = new MyErrorStateMatcher();
+
+  test = false;
+  latitude = 0;
+  longitude = 0;
   zoom: number;
   address: string;
   placeaddress;
@@ -43,18 +53,32 @@ export class SignupComponent implements OnInit {
   form2 = false;
   form3 = false;
   form4 = false;
-  @ViewChild('search',{static:false}) public searchElementRef: ElementRef;
+  searchElementRef: ElementRef;
+  @ViewChild('search') set content(content: ElementRef) {
+    if (content) { // initially setter gets called with undefined
+      this.searchElementRef = content;
+    }
+  }
 
 
   signForm1 = new FormGroup({
     fname: new FormControl('', [Validators.required]),
     lname: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl('', [Validators.required,Validators.maxLength(10), Validators.minLength(8)]),
   });
 
   signForm2 = new FormGroup({
+    nic: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.minLength(8)]),
     address: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.minLength(8)]),
+  });
+  signForm3 = new FormGroup({
+    address1: new FormControl('', [Validators.required]),
+  });
+
+  signForm4 = new FormGroup({
+    password: new FormControl('', [Validators.required]),
+    cpassword: new FormControl('', [Validators.required]),
   });
 
 
@@ -63,40 +87,22 @@ export class SignupComponent implements OnInit {
 
   constructor(
     private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone
+    private ngZone: NgZone,
+    private fb: FormBuilder,
   ) { }
 
 
 
   ngOnInit() {
-    // load Places Autocomplete
-    this.mapsAPILoader.load().then(() => {
-      this.setCurrentLocation();
-      this.geoCoder = new google.maps.Geocoder;
-      const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
-
-      autocomplete.addListener('place_changed', () => {
-        this.ngZone.run(() => {
-          // get the place result
-          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-          // verify result
-          if (place.geometry === undefined || place.geometry === null) {
-            return;
-          }
-
-          // set latitude, longitude and zoom
-          this.latitude = place.geometry.location.lat();
-          this.longitude = place.geometry.location.lng();
-          this.zoom = 15;
-        });
-      });
-    });
+    this.myform = this.fb.group({
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      cpassword: new FormControl(''),
+    }, { validator: this.checkPasswords });
   }
 
   // Get Current Location Coordinates
   private setCurrentLocation() {
-    if ('geolocation' in navigator) {
+    if ('geolocation' in navigator && (this.latitude === 0 && this.longitude === 0)) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
@@ -107,17 +113,18 @@ export class SignupComponent implements OnInit {
   }
 
 
+
   markerDragEnd($event: MouseEvent) {
     console.log($event);
     this.latitude = $event.coords.lat;
     this.longitude = $event.coords.lng;
+    this.test = true;
     this.getAddress(this.latitude, this.longitude);
   }
 
   getAddress(latitude, longitude) {
     this.geoCoder.geocode({ location: { lat: latitude, lng: longitude } }, (results, status) => {
-      console.log(results);
-      console.log(status);
+
       if (status === 'OK') {
         if (results[0]) {
           this.zoom = 15;
@@ -140,6 +147,15 @@ export class SignupComponent implements OnInit {
     }
   }
 
+
+  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+    const pass = group.get('password').value;
+    const confirmPass = group.get('cpassword').value;
+
+    return pass === confirmPass ? null : { notSame: true };
+  }
+
+
   changeForm(x){
     const elm1 = document.getElementsByClassName('btn1')
     if(x === 'form1'){
@@ -155,6 +171,9 @@ export class SignupComponent implements OnInit {
       this.btncu4 = false;
     } else if( x === 'form2'){
       if(this.signForm1.valid){
+        if (this.test){
+          this.user.address1 = this.user.address;
+        }
         this.form1 = false;
         this.form2 = true;
         this.form3 = false;
@@ -168,19 +187,53 @@ export class SignupComponent implements OnInit {
         this.btncu4 = false;
       }
     } else if ( x === 'form3'){
-      this.form2 = false;
-      this.form3 = true;
-      this.form4 = false;
+      if (this.signForm2.valid) {
+        console.log(this.user)
+        this.form2 = false;
+        this.form3 = true;
+        this.form4 = false;
 
-      this.btn1 = true;
-      this.btn2 = true;
-      this.btn3 = false;
-      this.btn4 = false;
-      this.btncu2 = false;
-      this.btncu3 = true;
-      this.btncu4 = false;
+        this.btn1 = true;
+        this.btn2 = true;
+        this.btn3 = false;
+        this.btn4 = false;
+        this.btncu2 = false;
+        this.btncu3 = true;
+        this.btncu4 = false;
+        setTimeout(() => {
+          this.mapsAPILoader.load().then(() => {
+            this.setCurrentLocation();
+            // tslint:disable-next-line: new-parens
+            this.geoCoder = new google.maps.Geocoder;
+
+            const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+
+
+            autocomplete.addListener('place_changed', () => {
+              this.ngZone.run(() => {
+                // get the place result
+                const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+                // verify result
+                if (place.geometry === undefined || place.geometry === null) {
+                  return;
+                }
+                console.log(autocomplete)
+                this.test = true;
+                // set latitude, longitude and zoom
+                this.latitude = place.geometry.location.lat();
+                this.longitude = place.geometry.location.lng();
+                this.zoom = 15;
+              });
+            });
+          });
+        }, 100)
+      }
 
     } else if ( x === 'form4'){
+      this.user.lat = this.latitude;
+      this.user.lon = this.longitude;
+      this.user.address1 = this.user.address;
       this.form3 = false;
       this.form4 = true;
 
@@ -194,4 +247,15 @@ export class SignupComponent implements OnInit {
     }
   }
 
+}
+
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
 }
