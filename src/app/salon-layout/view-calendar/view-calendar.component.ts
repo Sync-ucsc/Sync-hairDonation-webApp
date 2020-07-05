f58393ef1f7e6a23d6bdac6e939877195143a804
isuru1221 <isurusandaruwann1221@gmail.com>
2020-07-06 00:02:36 +0530

    fix of view-calendar

--- src/app/salon-layout/view-calendar/view-calendar.component.ts
+++ src/app/salon-layout/view-calendar/view-calendar.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
-import { FullCalendarComponent } from '@fullcalendar/angular';
-import { EventInput, Calendar } from '@fullcalendar/core';
+import {Component, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {FullCalendarComponent} from '@fullcalendar/angular';
+import {EventInput} from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGrigPlugin from '@fullcalendar/timegrid';
-import interactionPlugin, { Draggable } from '@fullcalendar/interaction'; // for dateClick
+import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
 import * as moment from 'moment';
-import {MatDialogModule} from '@angular/material/dialog';
-import {ViewCalendarService} from './../../service/viewcalendar.service';
-import { FormGroup, FormBuilder, FormControl,Validators } from '@angular/forms';
-
+import {ViewCalendarService} from '@services/viewcalendar.service';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators} from '@angular/forms';
 // ES6 Modules or TypeScript
 import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-view-calendar',
   templateUrl: './view-calendar.component.html',
@@ -21,22 +20,22 @@ export class ViewCalendarComponent implements OnInit {
   addForm: FormGroup;
   submitted = false;
 
-
-
+  // use for clear formFiled
+  @ViewChild(FormGroupDirective) formGroupDirective: FormGroupDirective;
 
   options: any;
   event: any;
   eventsModel: any;
   n: number;
   date: number;
-  @ViewChild('calendar', { static: false }) calendar: FullCalendarComponent; // the #calendar in the template
+  @ViewChild('calendar', {static: false}) calendar: FullCalendarComponent; // the #calendar in the template
 
   calendarVisible = true;
   // calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
   calendarWeekends = true;
   calendarEvents: EventInput[] = [
-    { title: 'Event Now', start: '2020-07-03T16:00:00' },
-    { title: 'Event Now', start: '2020-07-03T16:00:00' },
+    {title: 'Event Now', start: '2020-07-03T16:00:00'},
+    {title: 'Event Now', start: '2020-07-03T16:00:00'},
     {
       start: '2020-07-03T10:00:00',
       end: '2020-07-03T13:00:00',
@@ -44,11 +43,11 @@ export class ViewCalendarComponent implements OnInit {
       rendering: 'background'
     },
     {
-      title : 'Salon closed ',
-      start : '2020-07-04T08:00:00',
-      end   : '2019-07-04T17.00.00',
-      color : "#019efb"
-  },
+      title: 'Salon closed ',
+      start: '2020-07-04T08:00:00',
+      end: '2019-07-04T17.00.00',
+      color: '#019efb'
+    },
 
   ];
   todayDate = moment().startOf('day');
@@ -58,57 +57,58 @@ export class ViewCalendarComponent implements OnInit {
 
   // }
   taskForm: FormGroup;
+
   constructor(
     private fb: FormBuilder,
-    private renderer:Renderer2,
-    private _ViewCalendarService:ViewCalendarService,
-    
-    )
-     {
+    private renderer: Renderer2,
+    private _ViewCalendarService: ViewCalendarService,
+  ) {
     this.taskForm = fb.group({
-      taskName: [""]
+      taskName: ['']
     });
   }
 
- 
 
   get taskName() {
-    return this.taskForm.get("taskName") as FormControl;
+    return this.taskForm.get('taskName') as FormControl;
   }
 
+  get f() {
+    return this.addForm.controls;
+  }
 
-  show()
-  {
+  show() {
     this.showModal = true; // Show-Hide Modal Check
 
   }
-  // Modal Close event
-  onClick()
-  {
-    this.showModal = false;
-    //document.getElementById("imagemodal").style.display="hide";
-  }
 
- 
+  // Modal Close event
+  onClick() {
+    this.showModal = false;
+    document.getElementById('imagemodal').style.display = 'none';
+    console.log(`called me`)
+    this.calendarEvents.pop()
+    // document.getElementById("imagemodal").style.display="hide";
+  }
 
   ngOnInit() {
     this.addForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(6)]],
-      mobile: ['', [Validators.required, Validators.pattern(/^-?([0-9]\d*)?$/), Validators.minLength(10)]]
+      name: [' ', [Validators.required, Validators.minLength(6)]],
+      mobile: [' ', [Validators.required, Validators.pattern(/^-?([0-9]\d*)?$/), Validators.minLength(10)]]
     });
 
     this._ViewCalendarService.my().subscribe(
-      data=>{
+      data => {
         data.forEach(element => {
 
-          let event = {
-            id : element.id,
-            title : element.name,
-            date : element.date
+          const event = {
+            id: element.id,
+            title: element.name,
+            date: element.date
           }
           this.calendar.getApi().addEvent(event);
 
-        //  this.calendarEvents;
+          //  this.calendarEvents;
         });
 
       }
@@ -155,34 +155,34 @@ export class ViewCalendarComponent implements OnInit {
   }
 
   drop() {
-     alert('dropped!');
+    alert('dropped!');
   }
 
   handleDateClick(arg) {
-    document.getElementById("imagemodal").style .display="block";
-     this.calendarEvents = this.calendarEvents.concat({
-        //title: this.name,
-        name: this.onSubmit(),
-        start: arg.date,
-        allDay: arg.allDay
-     })
+    document.getElementById('imagemodal').style.display = 'block';
+    this.calendarEvents = this.calendarEvents.concat({
+      // title: this.name,
+      name: this.onSubmit(),
+      start: arg.date,
+      allDay: arg.allDay
+    })
 
 
-    //'show()';
-   // console.log(this.event);
-  //  swal({
-  //         title: 'Add Appointment',
-  //         html:
-  //             '<input id=" swal-input1" class="wal2-input">'+
-  //             '<input id="swal-input2" class=" swal2-input " >',
-  //         focusConfirm: false,
-  //         preConfirm: ()=>{
-  //           return [
-  //             document.getElementById('swal-input1').value,
-  //             document.getElementById('swal-input2').value
-  //           ]
-  //         }
-  //       })
+    // 'show()';
+    // console.log(this.event);
+    //  swal({
+    //         title: 'Add Appointment',
+    //         html:
+    //             '<input id=" swal-input1" class="wal2-input">'+
+    //             '<input id="swal-input2" class=" swal2-input " >',
+    //         focusConfirm: false,
+    //         preConfirm: ()=>{
+    //           return [
+    //             document.getElementById('swal-input1').value,
+    //             document.getElementById('swal-input2').value
+    //           ]
+    //         }
+    //       })
 
     // const swalWithBootstrapButtons = Swal.mixin({
     //   customClass: {
@@ -194,7 +194,7 @@ export class ViewCalendarComponent implements OnInit {
 
     // swalWithBootstrapButtons.fire({
     //   title: 'Add Appointment',
-     // text: "Add customer  Name:<input type ='text'> <br> Add  Customer Phone Number :<input type = 'telephone'>",
+    // text: "Add customer  Name:<input type ='text'> <br> Add  Customer Phone Number :<input type = 'telephone'>",
 
     //   input: 'text',
 
@@ -215,7 +215,7 @@ export class ViewCalendarComponent implements OnInit {
     //       'success'
     //     )
     //   } else if (
-        /* Read more about handling dismissals below */
+    /* Read more about handling dismissals below */
     //     result.dismiss === Swal.DismissReason.cancel
     //   ) {
     //     swalWithBootstrapButtons.fire(
@@ -227,9 +227,6 @@ export class ViewCalendarComponent implements OnInit {
     // })
 
 
-
-
-
     // if (confirm('Would you like to add appointment' + arg.dateStr + ' ?')) {
     //   this.event = prompt('Enter Event', '');
     //   console.log(this.event);
@@ -237,10 +234,9 @@ export class ViewCalendarComponent implements OnInit {
 //  title: this.event,
 //       start: arg.date,
 //         allDay: arg.allDay
-//})
+// })
 
-    }
-
+  }
 
   eventClick(model) {
     console.log(model);
@@ -250,14 +246,13 @@ export class ViewCalendarComponent implements OnInit {
     console.log(model);
   }
 
-
   createTask(event) {
     console.log(event.event)
-    //console.log('hi')
+    // console.log('hi')
     // console.dir(this.calendar.element.nativeElement.querySelector(".fc-event"))
-     let date = event.event.start;
-     date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-     let string = event.event.name;
+    let date = event.event.start;
+    date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    const string = event.event.name;
 
     //  this._ViewCalendarService.create(string, date).subscribe(
     //   data => {
@@ -268,16 +263,18 @@ export class ViewCalendarComponent implements OnInit {
     //  )
 
   }
+
   updateTask(event) {
 
-     console.log(event)
-     let id = (event.event.id) ? event.event.id : event.event._def.id;
-     let date = event.event.start;
-     date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    console.log(event)
+    const id = (event.event.id) ? event.event.id : event.event._def.id;
+    let date = event.event.start;
+    date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 
-     this._ViewCalendarService.update(id, date).subscribe(
-    data => { }
-     )
+    this._ViewCalendarService.update(id, date).subscribe(
+      data => {
+      }
+    )
   }
 
   eventDo(event) {
@@ -287,22 +284,6 @@ export class ViewCalendarComponent implements OnInit {
     this.renderer.appendChild(icon, close);
     this.renderer.appendChild(event.el, icon)
     this.renderer.addClass(event.el, 'text-light')
-  }
-  deleteEvent(event) {
-
-    if (event.jsEvent.srcElement.className == 'delete-icon') {
-      console.log("delete-icon")
-      console.log(event.event)
-      let id = (event.event.id) ? event.event.id : event.event._def.id;
-      console.log(id)
-       this._ViewCalendarService.delete(id).subscribe(
-         data => {
-         if (data) {
-           event.event.remove();
-          }
-         }
-       )
-    }
   }
 
   // get yearMonth(): number {
@@ -314,42 +295,59 @@ export class ViewCalendarComponent implements OnInit {
 
   // }
 
+  deleteEvent(event) {
+
+    if (event.jsEvent.srcElement.className == 'delete-icon') {
+      console.log('delete-icon')
+      console.log(event.event)
+      const id = (event.event.id) ? event.event.id : event.event._def.id;
+      console.log(id)
+      this._ViewCalendarService.delete(id).subscribe(
+        data => {
+          if (data) {
+            event.event.remove();
+          }
+        }
+      )
+    }
+  }
+
   dayRender(ev) {
     ev.el.addEventListener('dblclick', () => {
       alert('double click!');
     });
   }
 
-  get f() { return this.addForm.controls; }
+  onSubmit() {
 
+    this.submitted = true;
+    const formValue = this.addForm.value;
 
-  onSubmit(){
+    // clear form values
+    setTimeout(() => this.formGroupDirective.resetForm(), 0);
 
+    console.log(formValue)
 
-
-    console.log(this.addForm.value);
-    this.submitted=true;
-  
     if (!this.addForm.valid) {
-     return false;
-   } else {
+      return false;
+    } else {
 
-    // this._ViewCalendarService.createAppointment(this.addForm.value).subscribe(
-    //    data => {
-    //    console.log('Appointment succesfully added!'+data)
-        Swal.fire(
-           'Done!',
-              'You added a new appointment!',
-            'success'
-         )
-        // }
-       // this.showModal = false;
+      // this._ViewCalendarService.createAppointment(this.addForm.value).subscribe(
+      //    data => {
+      //    console.log('Appointment succesfully added!'+data)
+      Swal.fire(
+        'Done!',
+        'You added a new appointment!',
+        'success'
+      )
+      // }
+      // this.showModal = false;
 
       // );
       return name;
     }
 
 
-   }
-
   }
+
+}
