f8f7e8620f89ffb357bcc7abd8ee9631cdcd05f1
isuru1221 <isurusandaruwann1221@gmail.com>
2020-07-05 15:46:36 +0530

    changes of target service

--- src/app/service/target.service.ts
+++ src/app/service/target.service.ts
@@ -10,14 +10,35 @@ import {Observable} from 'rxjs';
 })
 export class TargetService {
 
+  salonId = `5f00ccbd22abe3315465b1cd`;
+
   baseUrl = `${environment.BASE_URL}/targets`
 
   constructor(private _http: HttpClient,
               private _sharedService: SharedService) {
   }
 
+  /**
+   * get all salons need to delivers
+   */
   getAllSalonNeedToDelivers(): Observable<any> {
     return this._http.get(`${this.baseUrl}/allSalonNeedToDelivers`)
+      .pipe(catchError(this._sharedService.httpErrorManagement))
+  }
+
+  /**
+   * get salon need to deliver by salonId\
+   */
+  getSalonNeedToDeliver(salonId: string): Observable<any>{
+    return this._http.get(`${this.baseUrl}/getSalonNeedToDelivers/${salonId}`)
+      .pipe(catchError(this._sharedService.httpErrorManagement))
+  }
+
+  /**
+   * add new need to deliver for salons NeedToDeliver object array
+   */
+  addNewNeedToDeliverToTheSalon(salonId: string): Observable<any>{
+    return this._http.put(`${this.baseUrl}/addNewDeliver/${salonId}`, undefined)
       .pipe(catchError(this._sharedService.httpErrorManagement))
   }
 }
