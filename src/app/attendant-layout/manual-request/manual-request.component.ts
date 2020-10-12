3ab27b49a1e92fd8593abce887bef9c723134855
tharinduvindula <tharinduvindula@gmail.com>
2020-10-12 09:33:32 +0530

    patient manual request complete

--- src/app/attendant-layout/manual-request/manual-request.component.ts
+++ src/app/attendant-layout/manual-request/manual-request.component.ts
@@ -56,7 +56,7 @@ export class ManualRequestComponent implements OnInit {
     nic: '',
     phone: '',
     address: '',
-    password: '',
+    password: 'User@123',
     role: 'patient',
     fingerprint: 0,
     fpcount: 0,
@@ -117,14 +117,13 @@ export class ManualRequestComponent implements OnInit {
   });
   mForm3 = new FormGroup({
     patientNumber: new FormControl('', [Validators.required]),
-    patientReport: new FormControl('', [Validators.required]),
     wigsType: new FormControl('', [Validators.required]),
   });
 
 
 
   valid(){
-    return !this.mForm3.valid || this.url === null
+    return !this.mForm3.valid
   }
   constructor(
     @Inject(AngularFireStorage) private storage: AngularFireStorage,
@@ -136,78 +135,10 @@ export class ManualRequestComponent implements OnInit {
     private userService: UserService,
     private fingerprint: FingerprintService
   ) {
-    this.user.fingerprint = getFingerprint()
-    this.fingerprint.checkfp(getFingerprint()).subscribe(
-      data => {
-        console.log(data)
-        if (data.success === true) {
-          if (data.data !== '') {
-            console.log(data.data);
-            if (data.data.block === true && data.data.check === false) {
-              this.router.navigate(['/']);
-              Swal.fire(
-                'Error!',
-                'you can creat only limited account!',
-                'error'
-              )
-
-            } else if (data.data.block === true && data.data.check === true) {
-              this.user.fpcount = 1
-              Swal.fire(
-                'warning!',
-                'you have alrady account!',
-                'warning'
-              )
-            } else {
-              this.user.fpcount = 1
-              Swal.fire(
-                'warning!',
-                'you have alrady one account!',
-                'warning'
-              )
-            }
-          }
-        } else {
-          this.router.navigate(['/']);
-          Swal.fire(
-            'error!',
-            'connection error!',
-            'error'
-          )
-        }
-      },
-      error => {
-        // Do something with error
-        this.router.navigate(['/'])
-        console.error(error);
-        Swal.fire(
-          'error!',
-          'connection error!',
-          'error'
-        )
-      }
-    )
+    this.selectedImage={name:''}
   }
 
   async ngOnInit(): Promise<void> {
-    console.log(this._token.getId())
-    this.lastRequestData = await this.getLastRequest()
-    console.log(this.lastRequestData)
-  }
-  async getLastRequest(): Promise<DbWigRequest> {
-    try {
-      const patientNumber = this._token.getId();
-
-      const response = await this._patientService.getLastRequest(patientNumber).toPromise() as BackendResponse;
-
-      if (!response.success) throw new Error(response.debugMessage)
-      const res = response.data as DbWigRequest;
-      if (!res.wigType) return null
-      return res
-
-    } catch (error) {
-      console.log(error)
-    }
   }
 
 
@@ -265,42 +196,6 @@ export class ManualRequestComponent implements OnInit {
   }
 
   async onSubmit(): Promise<void> {
-    try {
-
-      this.lastRequestData = await this.getLastRequest()
-
-      if (this.lastRequestData && !this.lastRequestData.finished && !this.lastRequestData.canceled) {
-        this._toastr.warning(`last request is still processing`);
-        this.Types = null;
-        return;
-      }
-
-      if (!this.Types) {
-        this._toastr.warning(`plz select type`);
-        return;
-      }
-
-      const patientNumber = this._token.getId();
-
-      const wigRequestObject = {
-        requestDay: new Date().toISOString(),
-        wigType: this.wigsType,
-        finished: false,
-        canceled: false,
-        attendantStatus: 0,
-      } as DbWigRequest;
-
-
-      const response = await this._patientService.createWigRequest(wigRequestObject, patientNumber).toPromise() as BackendResponse;
-      if (!response.success) throw new Error(response.debugMessage)
-
-      this.Types = null;
-      this.lastRequestData = await this.getLastRequest()
-      this._toastr.success(`wig request add successfully`)
-
-    } catch (error) {
-      this._toastr.warning(`fail to add wig request`)
-    }
 
       this.user1.password = Md5.hashStr(this.user.password).toString();
       this.user1.fname = this.user.fname;
@@ -312,30 +207,45 @@ export class ManualRequestComponent implements OnInit {
       this.user1.fingerprint = this.user.fingerprint;
       this.user1.fpcount = this.user.fpcount;
       this.user1.patientNumber = this.user.patientNumber;
-      this.userService.adduser(this.user1).subscribe(
-        data => {
-          console.log(data)
-          Swal.fire(
-            'Done!',
-            'Your Signup Successfuly!',
-            'success'
-          )
-          Swal.fire(
-            'Done!',
-            'Your Signup Successfuly!',
-            'success'
-          )
-          Swal.fire(
-            'Done!',
-            'Your Signup Successfuly!',
-            'success'
-          )
+      this.userService.patientadd(this.user1).subscribe(
+        async data => {
+          console.log(data['data'])
+          const wigRequestObject = {
+            requestDay: new Date().toISOString(),
+            wigType: this.wigsType,
+            finished: false,
+            canceled: false,
+            attendantStatus: 0,
+          } as DbWigRequest;
+          console.log(wigRequestObject);
 
-          this.router.navigate(['/patient-wait']);
+          // tslint:disable-next-line: max-line-length
+          const response = await this._patientService.createWigRequest(wigRequestObject, this.user.email).toPromise() as BackendResponse;
+          if (!response.success) throw new Error(response.debugMessage)
+          Swal.fire(
+            'Added!',
+            'Patient wig request added',
+            'success'
+          )
+          this.user = {
+            fname: '',
+            lname: '',
+            email: '',
+            nic: '',
+            phone: '',
+            address: '',
+            password: '',
+            role: 'patient',
+            fingerprint: 0,
+            fpcount: 0,
+            patientNumber: '',
+            patientReport: ''
+          }
+          this.form3 = false;
+          this.changeForm('form1');
         },
         error => {
           // Do something with error
-          this.router.navigate(['/'])
           console.error(error);
           Swal.fire(
             'error!',
